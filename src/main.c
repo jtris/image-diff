702ab7249d066de68ada07278c3a0adaada2d10b
triskj0 <tristreflis@gmail.com>
2025-09-09 00:21:44 +0200

    error handling

--- src/main.c
+++ src/main.c
@@ -9,6 +9,13 @@
 #include "stb_image.h"
 #include "stb_image_write.h"
 
+#define ERROR_MESSAGES_ENABLED 1
+#define ERROR_MESSAGE(...) do {                             \
+        if (ERROR_MESSAGES_ENABLED) {                       \
+            fprintf(stderr, "%s:%d : ", __FILE__, __LINE__);\
+            fprintf(stderr, __VA_ARGS__);                   \
+        }                                                   \
+    } while(0)
 #define ASSERT(_e, ...) if (!(_e)) { fprintf(stderr, __VA_ARGS__); exit(1); }
 
 typedef int Errno;
@@ -20,7 +27,8 @@ typedef struct {
 } image;
 
 
-Errno read_png(image *save_image, const char *filepath);
+Errno get_file_extension_from_path(const char *filepath, char *buffer);
+Errno read_png_or_jpg(image *save_image, const char *filepath); // TO-DO
 Errno export_as_png_or_jpg(image *save_image, const char *filepath);
 uint16_t get_two_pixel_difference(const uint8_t pixel1[3], const uint8_t pixel2[3]);
 Errno create_pixel_difference_heatmap(image *save_image, const image *image1, const image *image2);
@@ -37,44 +45,56 @@ int main()
 }
 
 
-Errno export_as_png_or_jpg(image *save_image, const char *filepath)
+Errno get_file_extension_from_path(const char *filepath, char *buffer)
 {
-    // get file extension
-    char file_extension[4];
     size_t filepath_length = 0;
     for (int i = 0; ; ++i) {
         if (filepath[i] == 0) break;
         filepath_length++;
     }
     
-    if (filepath_length <= 5) return 1;
+    if (filepath_length <= 5) { ERROR_MESSAGE("filepath is too short\n"); return 1; }
+    if (filepath[filepath_length-4] != '.') { ERROR_MESSAGE("couldn't find a file extension\n"); return 1; }
 
-    // load it into file_extension[]
-    // compare, etc.
+    // load last 3 characters into buffer
     for (int i = 0; i < 3; ++i) {
-        file_extension[i] = filepath[filepath_length-3+i];
+        buffer[i] = filepath[filepath_length-3+i];
     }
-    file_extension[3] = 0;
-
-    // png
-    if (strcmp(file_extension, "png") == 0) {
-        Errno err = stbi_write_png(filepath, save_image->width, save_image->height, 3, save_image->pixel_map, save_image->width*3);
-        if (err == 0) return 1; // 0 means failure here
-        return 0;
-    }
-
-    // jpg
-    Errno err = stbi_write_jpg(filepath, save_image->width, save_image->height, 3, save_image->pixel_map, 100);
-    if (err == 0) return 1; // 0 means failure here
+    buffer[3] = 0;
     return 0;
 }
 
 
-Errno read_png(image *save_image, const char *filepath)
+Errno export_as_png_or_jpg(image *save_image, const char *filepath)
+{
+    char file_extension[4] = {[3] = 0};
+    Errno err = get_file_extension_from_path(filepath, file_extension);
+    if (err) { ERROR_MESSAGE("an error occured while trying to read the file extension\n"); return 1; }
+
+    // png
+    if (strcmp(file_extension, "png") == 0) {
+        err = stbi_write_png(filepath, save_image->width, save_image->height, 3, save_image->pixel_map, save_image->width*3);
+        if (err == 0) { ERROR_MESSAGE("couldn't write a PNG image\n"); return 1; } // 0 means failure here
+        return 0;
+    }
+
+    // jpg
+    if (strcmp(file_extension, "jpg") == 0) {
+        err = stbi_write_jpg(filepath, save_image->width, save_image->height, 3, save_image->pixel_map, 100);
+        if (err == 0) { ERROR_MESSAGE("couldn't write a JPG image\n"); return 1; } // 0 means failure here
+        return 0;
+    }
+
+    ERROR_MESSAGE("save-path's file extension is neither of type PNG, nor JPG.\n");
+    return 1;
+}
+
+
+Errno read_png_or_jpg(image *save_image, const char *filepath)
 {
     int width, height;
     unsigned char *data = stbi_load(filepath, &width, &height, NULL, 3);
-    if (data == NULL) return 1;
+    if (data == NULL) { ERROR_MESSAGE("couldn't load file\n"); return 1; }
 
     save_image->width = width;
     save_image->height = height;
@@ -105,7 +125,7 @@ Errno create_pixel_difference_heatmap(image *save_image, const image *image1, co
 
     // compute all "pixel difference" values
     uint16_t *pixel_differences = malloc(save_image->resolution * sizeof(uint16_t));
-    if (!pixel_differences) return 1;
+    if (!pixel_differences) { ERROR_MESSAGE("could not allocate\n"); return 1; }
 
     uint16_t highest_difference_value = 0;
     for (size_t i = 0; i < save_image->resolution; ++i) {
@@ -117,7 +137,7 @@ Errno create_pixel_difference_heatmap(image *save_image, const image *image1, co
 
     // scale difference values to convert them to pixels
     uint8_t *scaled_pixel_differences = malloc(save_image->resolution * sizeof(uint8_t));
-    if (!scaled_pixel_differences) return 1;
+    if (!scaled_pixel_differences) { ERROR_MESSAGE("could not allocate\n"); return 1; }
 
     for (size_t p = 0; p < save_image->resolution; ++p) {
         scaled_pixel_differences[p] = 255 * (1.0f - ((float)pixel_differences[p] / (float)highest_difference_value));
@@ -155,7 +175,7 @@ Errno export_as_ppm(const image *save_image, const char *filepath)
 {
     FILE *f = NULL;
     f = fopen(filepath, "wb");
-    if (f == NULL) return 1;
+    if (f == NULL) { ERROR_MESSAGE("could not open file\n"); return 1; }
 
     // write header
     fprintf(f, "P6\n%zu %zu\n255\n", save_image->width, save_image->height); 
@@ -173,7 +193,7 @@ Errno read_ppm(image *save_image, const char *filepath)
 {
     FILE *f = NULL;
     f = fopen(filepath, "rb");
-    if (f == NULL) return 1;
+    if (f == NULL) { ERROR_MESSAGE("could not open file (%s)\n", filepath); return 1; }
 
     // verify the format (P6)
     char format_buffer[3];
@@ -211,6 +231,7 @@ Errno create_sample_ppm()
 
     f = fopen(filename, "wb");
     if (f == NULL) {
+        ERROR_MESSAGE("couldn't open file (%s)\n", filename);
         return 1;
     }
     
