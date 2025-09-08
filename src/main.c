431d587cb6cdf058f2e693cb876b0f1393114171
triskj0 <tristreflis@gmail.com>
2025-09-09 00:43:45 +0200

    build: move external libraries into /external
    and declare stb_image.h as a C file

--- src/main.c
+++ src/main.c
@@ -6,8 +6,8 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image.h"
-#include "stb_image_write.h"
+#include "external/stb_image.h"
+#include "external/stb_image_write.h"
 
 #define ERROR_MESSAGES_ENABLED 1
 #define ERROR_MESSAGE(...) do {                             \
